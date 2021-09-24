ef8d243361a5ab829baa21efa8309d5af45166b8
Bleakney Bissett <bleakneybissett@gmail.com>
2021-09-24 13:51:09 -0500

    create work component

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import Landing from '../Landing';
 import About from '../About';
+import Work from '../Work';
 
 function Main () {
-    const mainContent = [Landing, About];
+    const mainContent = [Landing, About, Work];
     const [currentContent, setContent] = useState(mainContent[0]);
     return (
         <main>
