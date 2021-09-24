d388238109b118e77a82d55ccc4ffa09d0613180
Bleakney Bissett <bleakneybissett@gmail.com>
2021-09-24 15:58:35 -0500

    add contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 
 function Contact () {
+
     return (
         <div className="contact-section">
             <div className="contact-form-container">
                 <h1>Contact</h1>
+                <form className="contact-form">
+                    <div className="mb-3">
+                        <label className="form-label" for="name-input">Name:</label>
+                        <input className="form-control" type="text" name="name-input" aria-label="name"></input>
+                        <label className="form-label" for="email-input">Email:</label>
+                        <input className="form-control" type="email" name="email-input" aria-label="email address"></input>
+                        <textarea className="form-control" rows="5" aria-label="personal message"></textarea>
+                    </div>
+                    <button className="btn" id="contactFormSubmission" type="submit">submit</button>
+                </form>
             </div>
         </div>
     )
