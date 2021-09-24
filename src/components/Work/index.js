e2b1845869fbc5ebf3793f677524a5dcb366a9db
Bleakney Bissett <bleakneybissett@gmail.com>
2021-09-24 17:00:20 -0500

    create carousel for work images

--- src/components/Work/index.js
+++ src/components/Work/index.js
@@ -3,7 +3,24 @@ import React from 'react';
 function Work () {
     return (
         <div className="work-section">
-
+            <div id="carouselExampleFade" className="carousel slide carousel-fade" data-bs-ride="carousel" data-bs-pause="false">
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+      <img src="../../images/broadcastr.png" default className="d-block w-100" alt="broadcastr app" />
+    </div>
+    <div className="carousel-item">
+      <img src="../../images/movie-date.png" default className="d-block w-100" alt="Movie Date app" />
+    </div>
+  </div>
+  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Previous</span>
+  </button>
+  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Next</span>
+  </button>
+</div>
         </div>
     )
 };
