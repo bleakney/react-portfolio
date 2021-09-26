d2be39e8fe728f72dc058806b6bbabe00f4f1dad
Bleakney Bissett <bleakneybissett@gmail.com>
2021-09-26 17:41:07 -0500

    fix broken img links and add gh-pages package

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,9 +4,9 @@ function Footer () {
     return (
         <footer>
             <div className="icons-container">
-           <a href="https://github.com/bleakney"><img src="../images/svg/github.svg" alt="github profile"></img></a>
-           <a href="https://www.linkedin.com/in/bleakney-bissett-737361a4/"> <img src="../images/svg/linkedin.svg" alt="linkedin profile"></img></a>
-            <a href="mailto:bleakneybissett@gmail.com"><img src="../images/svg/envelope.svg" alt="email me!" ></img></a>
+           <a href="https://github.com/bleakney"><img src="./images/svg/github.svg" alt="github profile"></img></a>
+           <a href="https://www.linkedin.com/in/bleakney-bissett-737361a4/"> <img src="./images/svg/linkedin.svg" alt="linkedin profile"></img></a>
+            <a href="mailto:bleakneybissett@gmail.com"><img src="./images/svg/envelope.svg" alt="email me!" ></img></a>
             </div>
         </footer>
     )
