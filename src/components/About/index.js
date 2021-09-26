410ac9ce7452ce28b58bf9d6dc7421aa5f4a1c77
Bleakney Bissett <bleakneybissett@gmail.com>
2021-09-26 14:40:58 -0500

    add picture and flexbox to about page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,9 +4,11 @@ function About () {
     return (
         <div className="about-page">
             <h1>About me</h1>
-            
+            <div class="about-container">
+            <img src="../images/me.jpg" />
             <p>Our end-to-end feature set is second to none, but our back-end performance and non-complex configuration is always considered a terrific achievement. The capacity to enable perfectly leads to the ability to whiteboard without lessening our power to deliver. What do we integrate? Anything and everything, regardless of humbleness! What does the commonly-accepted buzzword 'long-term' really mean? We believe we know that it is better to exploit wirelessly. We understand that if you incentivize dynamically, you may have to synergize wirelessly. Spriti introduced new capabilities to the ability to whiteboard without lessening our power to syndicate. Without niches, you will lack social networks. A company that can synthesize courageously will (eventually) be able to engineer virtually than to strategize macro-intuitively. Quick: do you have a plan to become proactive. It may seem confusing, but it's realistic! Imagine a combination of PGP and XSL. What does the term 'e-tailers' really mean? What do we harness? Anything and everything, regardless of obscureness! Our feature set is second to none, but our power to syndicate. Without micro-resource-constrained performance, you will lack versioning. We believe we know that if you drive micro-mega-cyber-virally then you may also reintermediate magnetically. Quick: do you have a virally-distributed plan of action for managing emerging partnerships? Without data hygiene supervising, you will lack research and development.
 </p>
+</div>
         </div>
     )
 }
